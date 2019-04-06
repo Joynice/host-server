5891234dfed4df11c58af9580df8af9751221ad3
Joynice <woshiliran@foxmai.com>
2019-04-06 12:52:52 +0800

    '提交文件'

---
+++ ven/Scripts/celeryd-multi-script.py
@@ -0,0 +1,12 @@
+#!D:\events\host-server\ven\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'celery==3.1.10','console_scripts','celeryd-multi'
+__requires__ = 'celery==3.1.10'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('celery==3.1.10', 'console_scripts', 'celeryd-multi')()
+    )
